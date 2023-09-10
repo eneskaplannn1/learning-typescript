11c836989b8cf3463ed84e386f4da8f8e8ecc2ae
eneskaplannn1 <muhammedenes.kaplan@std.yeditepe.edu.tr>
2023-09-10 21:20:14 +0300

    started learning typescript

---
+++ starting-typescript/build/js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+const username = "deneme2";
+console.log(username);
+let a = 5;
+let b = "5";
+let c = 12;
+let cd = 12;
+console.log(c / a);
---
+++ starting-typescript/src/main.ts
@@ -0,0 +1,9 @@
+const username = "deneme2";
+console.log(username);
+
+let a: number = 5;
+let b: string = "5";
+let c: number = 12;
+let cd: number = 12;
+
+console.log(c / a);
