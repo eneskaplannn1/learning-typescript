e08c727dfe97840a6f13d8f05c18533c4ff6b253
eneskaplannn1 <muhammedenes.kaplan@std.yeditepe.edu.tr>
2023-09-10 21:40:17 +0300

    learned giving type to variables

--- starting-typescript/src/main.ts
+++ starting-typescript/src/main.ts
@@ -1,9 +1,28 @@
-const username = "deneme2";
-console.log(username);
+const username: string = "deneme2";
 
 let a: number = 5;
 let b: string = "5";
 let c: number = 12;
 let cd: number = 12;
 
-console.log(c / a);
+// console.log(c / b);
+
+let name2: string;
+let isLoading: boolean;
+let secretNumber: number;
+let hero: any;
+let mixed: boolean | number;
+
+name2 = "Enes";
+isLoading = true;
+name2 = "Kaplan";
+secretNumber = 19;
+hero = "hello world";
+hero = 21;
+
+mixed = true;
+
+function accumulator(a: number, b: number) {
+  if (mixed) return a + b;
+  return a;
+}
